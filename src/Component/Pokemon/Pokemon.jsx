e2db60486f36e7f1a857b34b37d12c4b419976f8
satyajay01 <satyajaydibya0@gmail.com>
2024-04-13 14:36:43 +0530

    UI bug fixed and home page styling completed

---
+++ src/Component/Pokemon/Pokemon.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Pokemon.css";
+
+const Pokemon = ({ name, image }) => {
+  return (
+    <div className="pokemon">
+      <div className="pokemon-name">{name}</div>
+      <div><img className="pokemon-img" src={image} alt="" /></div>
+    </div>
+  );
+};
+
+export default Pokemon;
--- src/Component/PokemonList/PokemonList.jsx
+++ src/Component/PokemonList/PokemonList.jsx
@@ -1,48 +1,63 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import "./PokemonList.css";
+import Pokemon from "../Pokemon/Pokemon";
 
 const PokemonList = () => {
   const [pokemonList, setPokemonList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  const POKEDEX_URL = "https://pokeapi.co/api/v2/pokemon/";
+
   async function downloadPokemons() {
-    const response = await axios.get("https://pokeapi.co/api/v2/pokemon/");
+    const response = await axios.get(POKEDEX_URL); // list of 20 pokemon.
     // console.log(response);
 
-    const pokemonResults = response.data.results;
+    const pokemonResults = response.data.results; // this is the array of pokemons from result
     // console.log(pokemonResults);
-    const pokemonResultPromise = pokemonResults.map((pokemon) =>axios.get(pokemon.url)
-      // console.log(pokemonResultPromise)
-    );
 
-    const pokemonData = await axios.all(pokemonResultPromise);
+    const pokemonResultPromise = pokemonResults.map((pokemon) =>
+      axios.get(pokemon.url)
+    );
+    // console.log(pokemonResultPromise)
+
+    const pokemonData = await axios.all(pokemonResultPromise); // array of 20 pokemon detailed data
     // console.log(pokemonData);
 
-    const result = pokemonData.map((pokeData) => {
+    const pokeListResult = pokemonData.map((pokeData) => {
       const pokemon = pokeData.data;
       // console.log(pokemon);
       return {
+        id: pokemon.id,
         name: pokemon.name,
         image: pokemon.sprites.other.dream_world.front_default,
         types: pokemon.types,
       };
     });
 
-
-    console.log("20 Pokimon Data (Name, image, types)",result);
-    setPokemonList(result);
+    console.log("20 Pokemon Data (Id, Name, image, types)", pokeListResult);
+    setPokemonList(pokeListResult);
     setIsLoading(false);
   }
   useEffect(() => {
-
     downloadPokemons();
   }, []);
-  return;
-  <div className="pokemon-list-wrapper">
-    <div>Pokemon List</div>
-    {isLoading ? "Loading..." : "Data downloaded"}
-  </div>;
+
+  return (
+    <div className="pokemon-list-wrapper">
+      <div className="pokemon-wrapper">
+        {isLoading
+          ? "Loading..."
+          : pokemonList.map((p) => (
+              <Pokemon name={p.name} image={p.image} key={p.id} />
+            ))}
+      </div>
+      <div className="controls">
+        <button>Prev</button>
+        <button>Next</button>
+      </div>
+    </div>
+  );
 };
 
 export default PokemonList;
