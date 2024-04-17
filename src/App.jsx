b85bb44cba3f9fc5a7383db799b3a512ce036e39
satyajay01 <satyajaydibya0@gmail.com>
2024-04-17 10:57:53 +0530

    React router complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import CustomRoutes from './routes/CustomRoutes'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import CustomRoutes from "./routes/CustomRoutes";
+import "./App.css";
 
 const App = () => {
   return (
     <>
-     <CustomRoutes/>
+      <h1 id="pokedex-heading">
+        <Link className="pokedex-heading" to="/">
+          Pokedex
+        </Link>
+      </h1>
+      <CustomRoutes />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Component/PokemonDetails/PokemonDetails.jsx
+++ src/Component/PokemonDetails/PokemonDetails.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const PokemonDetails = () => {
-  return (
-    <div>Details</div>
-  )
-}
+  const { id } = useParams();
+  const [pokemon, setPokemon] = useState({});
+  console.log(id);
 
-export default PokemonDetails
+  async function downloadPokemon() {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    // console.log( response.data); //response.data
+
+    setPokemon({
+      name: response.data.name,
+      image: response.data.sprites.other.dream_world.front_default,
+      weight: response.data.weight,
+      height: response.data.height,
+      types: response.data.types.map((t) => t.type.name),
+    });
+  }
+
+  console.log(pokemon);
+
+  useEffect(() => {
+    downloadPokemon();
+  }, []);
+
+  return (
+    <>
+      <div className="pokemon-details-wrapper">
+        <div className="pokemon-detail-name">Name: {pokemon.name}</div>
+        <img
+          className="pokemon-detail-image"
+          src={pokemon.image}
+          alt="pokemon image"
+        />
+        <div>Height: {pokemon.height}</div>
+        <div>Weight: {pokemon.weight}</div>
+        <div className="pokemon-detail-types">
+          {pokemon.types && pokemon.types.map((t) => <div key={t}> {t} </div>)}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PokemonDetails;
